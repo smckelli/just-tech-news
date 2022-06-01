e27430088eefc59a47aade23b5bf50e5ffb8552f
smckelli <smckelli@gmail.com>
2022-06-01 13:32:30 -0500

    using Clean Code 14.3

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {}, 
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
